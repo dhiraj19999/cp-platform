de04809ef1d35fe41cad122e0a5ec3fb6c31bca4
dhiraj garad <yogeshsamse682@gmail.com>
2022-10-13 14:50:26 +0530

    add

---
+++ masai-ops-114-23-CaUgJi-c7c4bd4c7123ffc4c0759b0a526f06ed9f41311c/src/components/Fiction.jsx
@@ -0,0 +1,11 @@
+export default function Fiction() {
+  return (
+    <div data-testid='books-fiction'>
+      <h1 data-testid='books-container-title'>{}</h1>
+
+      <div className="books-container">
+        {/* Map all Fictional Books here */}
+      </div>
+    </div>
+  );
+}
